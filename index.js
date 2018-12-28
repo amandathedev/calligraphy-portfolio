38932b2501e47771e50c8b5ac57e6992b193018a
Amanda Treutler <amandathedev@gmail.com>
2018-12-28 10:22:20 -0600

    Added photos and jQuery

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-// $( '.menu-btn' ).click(function(){
-//     $('.responsive-menu').slideToggle('slow');
+// $( '.navbar-toggler' ).click(function(){
+//     $('.navbar-collapse').slideToggle('slow');
 // });
 
 // https://stackoverflow.com/questions/36721959/slow-opening-hamburger-menu
+
+
+console.log("hello");
+
+$(window).scroll(function(){
+	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
