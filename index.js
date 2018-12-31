5163de968ef57555c8404dd3a279ef1cf96f6712
Amanda Treutler <amandathedev@gmail.com>
2018-12-31 09:16:59 -0600

    Add image carousel

--- index.js
+++ index.js
@@ -4,9 +4,19 @@
 
 // https://stackoverflow.com/questions/36721959/slow-opening-hamburger-menu
 
-
 console.log("hello");
 
-$(window).scroll(function(){
-	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+// Add navbar color on scroll
+$(window).scroll(function() {
+  $("nav").toggleClass("scrolled", $(this).scrollTop() > 50);
 });
+
+// Change navbar font size on scroll
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("menu-items").style.fontSize = "1em";
+  } else {
+    document.getElementById("menu-items").style.fontSize = "1.2em";
+  }
+}
